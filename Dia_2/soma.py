d877acee407db4ce2f5ac1c9af66abd4cc7642bf
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-05 19:27:21 -0300

    criada a calculadora de soma do dia 2

---
+++ Dia_2/soma.py
@@ -0,0 +1,8 @@
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número para somar com o anterior: "))
+
+resultado  = num1 + num2
+
+print(f"O resultado é: {resultado}")
+
+
