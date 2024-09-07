379357e47ad19d0da03d080ec7c147b1b14dc3d6
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-07 19:35:57 -0300

    criada a base para calculadora simples, do dia 4

---
+++ Dia_4/CalculadoraSimples.py
@@ -0,0 +1,26 @@
+def adicao(x,y):
+    return x+y
+
+def subtracao(x,y):
+    return x-y
+
+def multiplicacao(x,y):
+    return x*y
+
+def divisao(x,y):
+    return x/y
+
+def calculadora();
+    print("Selecione a operacao.")
+    print("1.Adicao")
+    print("2.Subtracao")
+    print("3.Multiplicacao")
+    print("4.Divisao")
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+
+
+
+    
