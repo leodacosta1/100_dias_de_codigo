66c207e69a1bc2808d61dcdddddec07e7a88364f
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-19 23:03:52 -0300

    fiz estrutura de projeto com node + express

---
+++ Dia_46/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res,next) => {
+    const data = {
+        name: "Home"
+    }
+
+
+    res.render('index', data)
+})
+
+module.exports = router;
