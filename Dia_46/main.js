5aee0460193253bd1a9ea35862e32d38b44c87c1
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-20 23:03:55 -0300

    atualização do arquivo main.js

--- Dia_46/main.js
+++ Dia_46/main.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use('/', routes);
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hjs');
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(8080);
 console.log("Servidor rodando na porta 8080");
